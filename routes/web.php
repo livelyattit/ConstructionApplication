5c85f0033d00234b4b3e9e587e2f38de5da177f8
Affan <macbookpro2019@MacBooks-MacBook-Pro.local>
2023-04-01 18:06:02 +0500

    layout created

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-Route::group(['prefix' => 'admin'], function () {
+Route::group(['prefix' => 'developer'], function () {
     Voyager::routes();
 });
